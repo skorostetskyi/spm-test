40bb6831f34a5a8d1eddc5b57dbdcb8743151f6e
Skorostetskyi Sergii <sergii@skorostetskyi.com>
2022-05-17 17:35:05 +0200

    2.13.14

--- Package.swift
+++ Package.swift
@@ -11,16 +11,11 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "Lottie", url: "https://github.com/airbnb/lottie-ios.git", from: "3.2.1")  
+        .package(name: "Lottie", url: "https://github.com/airbnb/lottie-ios.git", from: "3.2.1"),
+        .package(name: "Passbase", url: "https://github.com/skorostetskyi/passbase-sdk-sp.git", from: "2.13.0"),
+        .package(name: "Microblink", url: "https://github.com/skorostetskyi/microblink-sdk-sp.git", from: "5.16.0")
     ],
     targets: [
-        .binaryTarget(name: "Passbase",
-                      url: "https://button.passbase.com/__ios/dev/Passbase_2.13.3.zip",
-                      checksum: "f2dba2bf148fd4faf9f4d073e042e13721de2cc1731b664f566d881e642c654c"),
-        .binaryTarget(name: "Microblink",
-                      url: "https://button.passbase.com/__ios/Microblink_5.16.1.zip",
-                      checksum: "33af45af2820e2bc0ff929ade3714c09c923ed03a1606d5219e16b9f87ddeeed"),
-
         .target(name: "PassbaseTargets",
                 dependencies: [
                     .product(name: "Lottie", package: "Lottie"),
